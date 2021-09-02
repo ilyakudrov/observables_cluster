a2f13b70c2a246969cc5ad379f80ea622cff4fdd
ilya <ilyakudrov@yandex.ru>
2021-09-02 20:20:20 +0300

    do1_wilson and do_wilson changed to distribute confs among different amount of jobs

--- scripts/do1_wilson.sh
+++ scripts/do1_wilson.sh
@@ -1,91 +1,127 @@
 #!/bin/bash
 conf_size="40^4"
 mu="0.05"
-smearing="HYP2_APE200"
-#smearing="unsmeared"
+conf_type="qc2dstag"
+HYP_steps=2
 
-#T_min=4
-#T_max=10
-#R_min="4.9"
-#R_max=20
+for mu in "0.05" "0.35" "0.45"
+do
 
-source "/lustre/rrcmpi/kudrov/conf/${conf_size}/mu${mu}/parameters"
+source "/lustre/rrcmpi/kudrov/conf/${conf_type}/${conf_size}/mu${mu}/parameters"
+script_path="/home/clusters/rrcmpi/kudrov/observables_cluster/scripts/do_wilson.sh"
 
-#conf_format="double_fortran"
-#conf_format="float_fortran"
-conf_format="double"
-#matrix_type="abelian"
-#matrix_type="su2"
-monopole="monopoless"
-#monopole=""
-#monopole="monopole"
+#conf_start=( 201 )
+#conf_end=( 201 )
+
+for monopole in "/" "monopole" "monopoless"
+#for monopole in "monopoless"
+do
+
+if [[ $monopole == "/" ]] ; then
+
+matrix_type="su2"
+conf_format="double_qc2dstag"
+smearing="HYP${HYP_steps}_APE${APE_steps_wilson}"
+
+elif [[ $monopole == "monopole" ]] ; then
+
+matrix_type="abelian"
+conf_format="double_fortran"
+smearing="unsmeared"
+
+elif [[ $monopole == "monopoless" ]] ; then
+
+matrix_type="su2"
+conf_format="double_fortran"
+smearing="HYP${HYP_steps}_APE${APE_steps_decomposition_wilson}"
+
+else
+
+echo wrong monopole ${monopole}
+
+fi
+
 axis="off-axis"
+#axis="on-axis"
 
-#mu0.05
-#chains=( "s0" "s1" "s2" "s3" "s4" )
-#conf_start=( 201 0 0 0 0 )
-#conf_end=( 637 436 423 433 419 )
+calculate_absent=true
 
-#chains=( "s2" "s3" "s4" )
-#conf_start=( 0 0 0 )
-#conf_end=( 423 433 419 )
-
-#chains=( "" )
-#conf_start=( 31 )
-#conf_end=( 400 )
+if [[ $conf_size == "40^4" ]] ; then
 
 T_min=4
 T_max=20
 R_min="4.9"
 R_max="20.1"
 
-#T_min=4
-#T_max=5
-#R_min="4.9"
-#R_max="6.1"
+elif [[ $conf_size == "32^4" ]] ; then 
 
-script_path="/home/clusters/rrcmpi/kudrov/observables_cluster/scripts/do_wilson.sh"
+T_min=4
+T_max=16
+R_min="4.9"
+R_max="16.1"
 
-for j in "${!chains[@]}"; do
+else
 
-for((i=${conf_start[j]};i<=${conf_end[j]};i++))
-do
-
-a=$(($i/1000))
-b=$((($i-$a*1000)/100))
-c=$((($i-$a*1000-$b*100)/10))
-d=$(($i-$a*1000-$b*100-$c*10))
-
-log_path="/home/clusters/rrcmpi/kudrov/observables_cluster/logs/wilson_loop/${axis}/${monopole}/qc2dstag/${conf_size}/mu${mu}/${chains[j]}"
-#log_path="/home/clusters/rrcmpi/kudrov/observables_cluster/logs/wilson_loop/${axis}/${monopole}/su2_dynam/${conf_size}/mu${mu}"
-mkdir -p ${log_path}
-
-#conf_path="/home/clusters/rrcmpi/kudrov/conf/${conf_size}/mu${mu}/${chains[j]}/confs/CONF$a$b$c$d"
-#conf_path="/home/clusters/rrcmpi/kudrov/decomposition/confs_decomposed/${monopole}/qc2dstag/${conf_size}/mu${mu}/${chains[j]}/conf_${monopole}_$a$b$c$d"
-conf_path="/home/clusters/rrcmpi/kudrov/smearing_cluster/confs_smeared/${monopole}/qc2dstag/${conf_size}/${smearing}/mu${mu}/${chains[j]}/conf_APE_alpha=0.7_$a$b$c$d"
-#conf_path="/home/clusters/01/vborn/Copy_from_lustre/SU2_dinam/MAG/mu0p0_b1p8_m0p0075_lam0p00075/MON_WL/CON_MON_MAG_$b$c$d.LAT"
-#conf_path="/home/clusters/rrcmpi/kudrov/smearing_cluster/confs_smeared/${monopole}/su2_dynam/${conf_size}/${smearing}/mu${mu}/${chains[j]}/conf_APE_alpha=0.7_$a$b$c$d"
-
-#output_path_wilson="/home/clusters/rrcmpi/kudrov/observables_cluster/result/wilson_loop/${conf_type}/${conf_size}/${smearing}/mu${mu}/${chains[j]}"
-output_path_wilson="/home/clusters/rrcmpi/kudrov/observables_cluster/result/wilson_loop/${axis}/${monopole}/${conf_type}/${conf_size}/mu${mu}/${chains[j]}"
-#output_path_wilson="/home/clusters/rrcmpi/kudrov/observables_cluster/result/wilson_loop/${axis}/su2_dynam/${monopole}/${conf_size}/${smearing}"
-#output_path_wilson="/home/clusters/rrcmpi/kudrov/observables_cluster/result/wilson_loop/test/${axis}/${monopole}/${conf_type}/${conf_size}/${smearing}/mu${mu}/${chains[j]}"
-mkdir -p ${output_path_wilson}
-output_path_wilson="${output_path_wilson}/wilson_loop_$a$b$c$d"
-
-if [ -f ${conf_path} ] ; then
-
-qsub -q long -v conf_format=${conf_format},conf_path=${conf_path},output_path_wilson=${output_path_wilson},matrix_type=${matrix_type},\
-L_spat=${L_spat},L_time=${L_time},T_min=${T_min},T_max=${T_max},R_min=${R_min},R_max=${R_max},axis=${axis}\
- -o "${log_path}/$a$b$c$d.o" -e "${log_path}/$a$b$c$d.e" ${script_path}
-while [ $? -ne 0 ]
-do
-qsub -q long -v conf_format=${conf_format},path_conf=${path_conf},output_path_wilson=${output_path_wilson},matrix_type=${matrix_type},\
-L_spat=${L_spat},L_time=${L_time},T_min=${T_min},T_max=${T_max},R_min=${R_min},R_max=${R_max},axis=${axis}\
- -o "${log_path}/$a$b$c$d.o" -e "${log_path}/$a$b$c$d.e" ${script_path}
-done
+echo wrong conf_size ${conf_size}
 
 fi
 
+
+number_of_jobs=100
+confs_total=0
+
+for i  in ${!chains[@]} ; do
+confs_total=$(( $confs_total + ${conf_end[$i]} - ${conf_start[$i]} + 1 ))
 done
+
+confs_per_job=$(( ${confs_total} / ${number_of_jobs} + 1 ))
+chain_current=0
+jobs_done=0
+
+for((i=0;i<${number_of_jobs};i++))
+do
+
+conf1=$(( ${conf_start[$chain_current]} + ${confs_per_job} * ($i - ${jobs_done}) ))
+conf2=$(( ${conf_start[$chain_current]} + ${confs_per_job} * ($i - ${jobs_done} + 1) - 1 ))
+
+if [[ $conf2 -ge ${conf_end[$chain_current]} ]] ; then
+conf2=$(( ${conf_end[$chain_current]} ))
+fi
+
+if [[ $chain_current -ge ${#chains[@]} ]] ; then
+break
+fi
+
+log_path="/home/clusters/rrcmpi/kudrov/observables_cluster/logs/wilson_loop/${axis}/${monopole}/${conf_type}/${conf_size}/mu${mu}/${chains[${chain_current}]}"
+mkdir -p ${log_path}
+
+a1=$((${conf1}/1000))
+b1=$(((${conf1}-$a1*1000)/100))
+c1=$(((${conf1}-$a1*1000-$b1*100)/10))
+d1=$((${conf1}-$a1*1000-$b1*100-$c1*10))
+
+a2=$((${conf2}/1000))
+b2=$(((${conf2}-$a2*1000)/100))
+c2=$(((${conf2}-$a2*1000-$b2*100)/10))
+d2=$((${conf2}-$a2*1000-$b2*100-$c2*10))
+
+qsub -q long -v conf_format=${conf_format},matrix_type=${matrix_type},calculate_absent=${calculate_absent},chain=${chains[$chain_current]},monopole=${monopole},mu=${mu},conf1=${conf1},conf2=${conf2},\
+L_spat=${L_spat},L_time=${L_time},T_min=${T_min},T_max=${T_max},R_min=${R_min},R_max=${R_max},axis=${axis},conf_size=${conf_size},conf_type=${conf_type},smearing=${smearing}\
+ -o "${log_path}/$a1$b1$c1$d1-$a2$b2$c2$d2.o" -e "${log_path}/$a1$b1$c1$d1-$a2$b2$c2$d2.e" ${script_path}
+while [ $? -ne 0 ]
+do
+qsub -q long -v conf_format=${conf_format},matrix_type=${matrix_type},calculate_absent=${calculate_absent},chain=${chains[$chain_current]},monopole=${monopole},mu=${mu},conf1=${conf1},conf2=${conf2},\
+L_spat=${L_spat},L_time=${L_time},T_min=${T_min},T_max=${T_max},R_min=${R_min},R_max=${R_max},axis=${axis},conf_size=${conf_size},conf_type=${conf_type},smearing=${smearing}\
+ -o "${log_path}/$a1$b1$c1$d1-$a2$b2$c2$d2.o" -e "${log_path}/$a1$b1$c1$d1-$a2$b2$c2$d2.e" ${script_path}
+done
+
+if [[ $conf2 -ge ${conf_end[$chain_current]} ]] ; then
+chain_current=$(( $chain_current + 1 ))
+jobs_done=$(( ${i} + 1 ))
+fi
+
+done
+
+done
+
 done
--- scripts/do_wilson.sh
+++ scripts/do_wilson.sh
@@ -1,4 +1,43 @@
 #!/bin/bash
+
+for((i=$conf1;i<=${conf2};i++))
+do
+
+a=$(($i/1000))
+b=$((($i-$a*1000)/100))
+c=$((($i-$a*1000-$b*100)/10))
+d=$(($i-$a*1000-$b*100-$c*10))
+
+conf_path_qc2dstag="/home/clusters/rrcmpi/kudrov/smearing_cluster/confs_smeared/qc2dstag/${conf_size}/mu${mu}/${smearing}/${chains[j]}/conf_APE_alpha=0.7_$a$b$c$d"
+conf_path_monopole="/home/clusters/rrcmpi/kudrov/decomposition/confs_decomposed/monopole/qc2dstag/${conf_size}/mu${mu}/$chain/conf_monopole_$a$b$c$d"
+conf_path_monopoless="/home/clusters/rrcmpi/kudrov/smearing_cluster/confs_smeared/monopoless/qc2dstag/${conf_size}/mu${mu}/${smearing}/${chains[j]}/conf_APE_alpha=0.7_$a$b$c$d"
+
+if [[ ${monopole} == "/" ]] ; then
+
+conf_path=$conf_path_qc2dstag
+
+else
+
+conf_path=("${!path1}")
+
+fi
+
+echo $conf_path
+
+if [ -f ${conf_path} ] ; then
+
+output_path_wilson="/home/clusters/rrcmpi/kudrov/observables_cluster/result/wilson_loop/${axis}/${monopole}/${conf_type}/${conf_size}/mu${mu}/$chain"
+mkdir -p ${output_path_wilson}
+output_path_wilson="${output_path_wilson}/wilson_loop_$a$b$c$d"
+
+if [ ! -f ${output_path_wilson} ] || [  ! ${calculate_absent} ] ; then
+
 parameters="-conf_format $conf_format -conf_path $conf_path -output_path_wilson $output_path_wilson -L_spat ${L_spat} -L_time ${L_time} -T_min ${T_min} -T_max ${T_max} -R_min ${R_min} -R_max ${R_max}"
 
 /home/clusters/rrcmpi/kudrov/observables_cluster/code/exe/wilson_${axis}_${matrix_type} $parameters
+
+fi
+
+fi
+
+done
