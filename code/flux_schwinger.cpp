546780ff70360d25ee0dbfdddb6633d8f1a3d8d4
Ilya <ilyakudrov@yandex.ru>
2021-09-05 17:55:02 +0300

    flux_schwinger added

---
+++ code/flux_schwinger.cpp
@@ -0,0 +1,190 @@
+#include "basic_observables.h"
+#include "data.h"
+#include "flux_tube.h"
+#include "link.h"
+#include "matrix.h"
+#include "result.h"
+
+#include <iostream>
+#include <map>
+
+int x_size;
+int y_size;
+int z_size;
+int t_size;
+
+int main(int argc, char *argv[]) {
+  unsigned int start_time;
+  unsigned int end_time;
+  unsigned int search_time;
+
+  std::string conf_format;
+  std::string smeared_format;
+  std::string conf_path;
+  std::string smeared_path;
+  std::string output_path_electric;
+  std::string output_path_magnetic;
+  int L_spat, L_time;
+  int R, T;
+  int x_trans;
+  // int T_min, T_max;
+  // FLOAT R_min, R_max;
+  for (int i = 1; i < argc; i++) {
+    if (std::string(argv[i]) == "-conf_format") {
+      conf_format = argv[++i];
+    } else if (std::string(argv[i]) == "-smeared_format") {
+      smeared_format = argv[++i];
+    } else if (std::string(argv[i]) == "-conf_path") {
+      conf_path = argv[++i];
+    } else if (std::string(argv[i]) == "-smeared_path") {
+      smeared_path = argv[++i];
+    } else if (std::string(argv[i]) == "-output_path_electric") {
+      output_path_electric = argv[++i];
+    } else if (std::string(argv[i]) == "-output_path_magnetic") {
+      output_path_magnetic = argv[++i];
+    } else if (std::string(argv[i]) == "-L_spat") {
+      L_spat = stoi(std::string(argv[++i]));
+    } else if (std::string(argv[i]) == "-L_time") {
+      L_time = stoi(std::string(argv[++i]));
+    } else if (std::string(argv[i]) == "-R_size") {
+      R = stoi(std::string(argv[++i]));
+    } else if (std::string(argv[i]) == "-T_size") {
+      T = stoi(std::string(argv[++i]));
+    } else if (std::string(argv[i]) == "-x_trans") {
+      x_trans = stoi(std::string(argv[++i]));
+    }
+    // else if (std::string(argv[i]) == "-T_min") { T_min =
+    // stoi(std::string(argv[++i])); } else if (std::string(argv[i]) ==
+    // "-T_max") { T_max = stoi(std::string(argv[++i])); } else if
+    // (std::string(argv[i]) ==
+    // "-R_min") { R_min = stoi(std::string(argv[++i])); } else if
+    // (std::string(argv[i]) == "-R_max") { R_max =
+    // stoi(std::string(argv[++i]));
+    // }
+  }
+
+  std::cout << "conf_format " << conf_format << std::endl;
+  std::cout << "smeared_format " << smeared_format << std::endl;
+  std::cout << "conf_path " << conf_path << std::endl;
+  std::cout << "smeared_path " << smeared_path << std::endl;
+  std::cout << "output_path_electric " << output_path_electric << std::endl;
+  std::cout << "output_path_magnetic " << output_path_magnetic << std::endl;
+  std::cout << "L_spat " << L_spat << std::endl;
+  std::cout << "L_time " << L_time << std::endl;
+  std::cout << "R " << R << std::endl;
+  std::cout << "T " << T << std::endl;
+  std::cout << "x_trans " << x_trans << std::endl;
+  // std::cout<<"T_min "<<T_min<<std::endl;
+  // std::cout<<"T_max "<<T_max<<std::endl;
+  // std::cout<<"R_min "<<R_min<<std::endl;
+  // std::cout<<"R_max "<<R_max<<std::endl;
+
+  x_size = L_spat;
+  y_size = L_spat;
+  z_size = L_spat;
+  t_size = L_time;
+
+  int d_min = -5;
+  int d_max = R + 5;
+
+  data<MATRIX> conf;
+  data<MATRIX> smeared;
+
+  if (std::string(conf_format) == "float") {
+    conf.read_float(conf_path);
+  } else if (std::string(conf_format) == "double") {
+    conf.read_double(conf_path);
+  } else if (std::string(conf_format) == "double_fortran") {
+    conf.read_double_fortran(conf_path);
+  } else if (std::string(conf_format) == "float_fortran") {
+    conf.read_float_fortran(conf_path);
+  } else if (std::string(conf_format) == "double_qc2dstag") {
+    conf.read_double_qc2dstag(conf_path);
+  }
+
+  if (std::string(smeared_format) == "float") {
+    smeared.read_float(smeared_path);
+  } else if (std::string(smeared_format) == "double") {
+    smeared.read_double(smeared_path);
+  } else if (std::string(conf_format) == "double_fortran") {
+    smeared.read_double_fortran(smeared_path);
+  } else if (std::string(conf_format) == "float_fortran") {
+    smeared.read_float_fortran(smeared_path);
+  } else if (std::string(conf_format) == "double_qc2dstag") {
+    smeared.read_double_qc2dstag(smeared_path);
+  }
+  double c1 = plaket_time(conf.array);
+  double c2 = plaket_space(conf.array);
+
+  double a;
+  double aver[2];
+  result vec(0);
+  result vec_plaket_time;
+  result vec_plaket_space;
+
+  start_time = clock();
+
+  std::vector<std::vector<std::vector<MATRIX>>> schwinger_lines_short;
+  for (int d = 1; d <= R + 5; d++) {
+    schwinger_lines_short.push_back(
+        calculate_schwinger_lines_short(conf.array, d));
+  }
+
+  std::vector<MATRIX> plaket_electric =
+      calculate_plaket_schwinger_time(conf.array);
+
+  std::vector<FLOAT> schwinger_electric = wilson_plaket_schwinger_electric(
+      smeared.array, schwinger_lines_short, plaket_electric, -5, R + 5, T, R);
+
+  schwinger_lines_short.clear();
+  plaket_electric.clear();
+
+  end_time = clock();
+  search_time = end_time - start_time;
+  std::cout << "T=" << T << " R=" << R
+            << " calculating time: " << search_time * 1. / CLOCKS_PER_SEC
+            << std::endl;
+
+  std::ofstream stream_electric;
+  // std::ofstream stream_magnetic;
+
+  stream_magnetic.precision(17);
+
+  stream_electric.open(output_path_electric);
+  // stream_magnetic.open(output_path_magnetic);
+
+  stream_electric << "d,wilson-schwinger-plaket-correlator,wilson-"
+                     "plaket-correlator,wilson-loop"
+                  << std::endl;
+
+  std::vector<FLOAT> vec_plaket_time = calculate_plaket_time_tr(conf.array);
+  std::vector<FLOAT> wilson_loop_tr =
+      calculate_wilson_loop_tr(smeared.array, R, T);
+
+  FLOAT wilson_loop_tr_average = 0;
+  for (int i = 0; i < wilson_loop_tr.size(); i++) {
+    wilson_loop_tr_average += wilson_loop_tr[i];
+  }
+  wilson_loop_tr_average = wilson_loop_tr_average / wilson_loop_tr.size();
+
+  std::map<int, FLOAT> wilson_plaket_correlator =
+      wilson_plaket_correlator_electric(wilson_loop_tr, vec_plaket_time, R, T,
+                                        x_trans, -5, R + 5);
+
+  // stream_magnetic << "d,wilson-plaket-correlator,wilson-loop,plaket"
+  //                 << std::endl;
+
+  for (int i = 0; i < schwinger_electric.size(); i++) {
+    stream_electric << i - 5 << "," << schwinger_electric[i] << ","
+                    << wilson_plaket_correlator[i - 5] << ","
+                    << wilson_loop_tr_average << std::endl;
+  }
+
+  // for (int i = 0; i < res2.size(); i++) {
+  //   stream_magnetic << d_min + i << "," << res2[i] << "," << b << "," << c2
+  //                   << std::endl;
+  // }
+
+  stream_electric.close();
+  // stream_magnetic.close();
+}
