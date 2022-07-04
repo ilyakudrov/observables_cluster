621cd6e8206268712f612f8b0bd2d05b7315eb20
ilya <ilyakudrov@yandex.ru>
2022-07-04 23:45:53 +0300

    minor changes

--- scripts/do_mag.sh
+++ scripts/do_mag.sh
@@ -13,13 +13,17 @@ path_conf="${conf_path_start}`printf %0${padding}d $i`${conf_path_end}"
 
 if [ -f ${path_conf} ] && [ -s ${path_conf} ] ; then
 
-mkdir -p ${output_path}
+mkdir -p "${output_path}/confs_gaugefixed"
+mkdir -p "${output_path}/confs_spin"
+mkdir -p "${output_path}/confs_gaugefixed"
+mkdir -p "${output_path}/functionals"
+
 path_conf_output="${output_path}/confs_gaugefixed/conf_`printf %04d $i`"
 path_spins_output="${output_path}/confs_spin/spins_`printf %04d $i`"
 path_previous="${output_path}/confs_spin/spins_`printf %04d $i`"
 path_functional_output="${output_path}/functionals/functional_`printf %04d $i`"
 
-parameters="-conf_format ${conf_format} -path_conf $path_conf -conf_format ${conf_format} -conf_format ${conf_format} -bites_skip ${bites_skip} \
+parameters="-conf_format ${conf_format} -path_conf $path_conf -conf_format ${conf_format} -bytes_skip ${bites_skip} \
     -path_spins_output ${path_spins_output} -path_conf_output ${path_conf_output} -path_previous ${path_previous} -path_functional_output ${path_functional_output}\
     -T_step ${T_step} -T_init ${T_init} -T_final ${T_final} -OR_steps ${OR_steps} -thermalization_steps ${thermalization_steps}\
     -tolerance_maximal ${tolerance_maximal} -tolerance_average ${tolerance_average} -tolerance_digits ${tolerance_digits} -gauge_copies ${gauge_copies} -is_new_trial ${is_new_trial}\
--- scripts/python/do1_mag.py
+++ scripts/python/do1_mag.py
@@ -5,22 +5,22 @@ from iterate_confs import *
 import subprocess
 import os
 
-conf_size = "24^4"
-# conf_size = "40^4"
-# conf_size = "48^4"
-conf_type = "su2_suzuki"
-# conf_type = "qc2dstag"
+#conf_size = "24^4"
+conf_size = "40^4"
+#conf_size = "48^4"
+#conf_type = "su2_suzuki"
+conf_type = "qc2dstag"
 theory_type = "su2"
 
-T_step = 0.1
+T_step = 0.0005
 T_init = 2.5
-T_final = 0.4
-OR_steps = 6
-thermalization_steps = 10
+T_final = 0.0005
+OR_steps = 4
+thermalization_steps = 50
 tolerance_maximal = 1e-8
 tolerance_average = 1e-11
 tolerance_digits = 7
-gauge_copies = 5
+gauge_copies = 10
 is_new_trial = 1
 is_final = 0
 is_compare = 1
@@ -29,15 +29,15 @@ is_functional_save = 1
 
 number_of_jobs = 50
 
-arch = "rrcmpi"
+arch = "rrcmpi-a"
 
-# for beta in ['/']:
-for beta in ['beta2.4']:
+for beta in ['/']:
+#for beta in ['beta2.8']:
     # for beta in ['beta2.4', 'beta2.5', 'beta2.6']:
     # for beta in ['beta2.4']:
-    # for mu in ['mu0.00', 'mu0.05', 'mu0.20', 'mu0.25', 'mu0.30', 'mu0.35', 'mu0.45']:
-    # for mu in ['mu0.25']:
-    for mu in ['/']:
+    #for mu in ['mu0.00', 'mu0.05', 'mu0.20', 'mu0.25', 'mu0.30', 'mu0.35', 'mu0.45']:
+    for mu in ['mu0.05', 'mu0.45']:
+    #for mu in ['/']:
 
         f = open(
             f'/home/clusters/rrcmpi/kudrov/conf/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/parameters.json')
@@ -52,24 +52,27 @@ for beta in ['beta2.4']:
         padding = data['padding']
         conf_name = data['conf_name']
 
-        # chains = {'/': [1, 1]}
-        # jobs = distribute_jobs(chains, number_of_jobs)
-        jobs = distribute_jobs(data['chains'], number_of_jobs)
+        #chains = {'/': [1, 50]}
+        chains = {'s0': [201, 250]}
+        jobs = distribute_jobs(chains, number_of_jobs)
+        #jobs = distribute_jobs(data['chains'], number_of_jobs)
 
         for job in jobs:
 
-            log_path = f'/home/clusters/rrcmpi/kudrov/observables_cluster/logs/mag/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/{job[0]}'
+            log_path = f'/home/clusters/rrcmpi/kudrov/observables_cluster/logs/mag/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/'\
+                f'T_step={T_step}/T_final={T_final}/OR_steps={OR_steps}/{job[0]}'
             conf_path_start1 = f'{conf_path_start}/{job[0]}/{conf_name}'
             try:
                 os.makedirs(log_path)
             except:
                 pass
-            output_path = f'/home/clusters/rrcmpi/kudrov/observables_cluster/result/mag/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/{job[0]}'
+            output_path = f'/home/clusters/rrcmpi/kudrov/observables_cluster/result/mag/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/'\
+                f'T_step={T_step}/T_final={T_final}/OR_steps={OR_steps}/{job[0]}'
             # qsub -q mem8gb -l nodes=1:ppn=4
-            bashCommand = f'qsub -q long -v conf_path_start={conf_path_start1},conf_path_end={conf_path_end},padding={padding},matrix_type={matrix_type},conf_format={conf_format},conf_format={conf_format},'\
-                f'T_step={T_step},T_init={T_init},T_final={T_final},OR_steps={OR_steps},thermalization_steps={thermalization_steps},'\
+            bashCommand = f'qsub -q long -v conf_path_start={conf_path_start1},conf_path_end={conf_path_end},padding={padding},matrix_type={matrix_type},conf_format={conf_format},bites_skip={bites_skip},'\
+                f'T_step={T_step},T_init={T_init},T_final={T_final},OR_steps={OR_steps},thermalization_steps={thermalization_steps},output_path={output_path},'\
                 f'tolerance_maximal={tolerance_maximal},tolerance_average={tolerance_average},tolerance_digits={tolerance_digits},gauge_copies={gauge_copies},is_new_trial={is_new_trial},'\
-                f'is_final={is_final},is_compare={is_compare},is_compare_spins={is_compare_spins},is_functional_save={is_functional_save},L_spat={L_spat},L_time={L_time}'\
+                f'is_final={is_final},is_compare={is_compare},is_compare_spins={is_compare_spins},is_functional_save={is_functional_save},L_spat={L_spat},L_time={L_time},'\
                 f'chain={job[0]},conf_start={job[1]},conf_end={job[2]},arch={arch}'\
                 f' -o {log_path}/{job[1]:04}-{job[2]:04}.o -e {log_path}/{job[1]:04}-{job[2]:04}.e /home/clusters/rrcmpi/kudrov/observables_cluster/scripts/do_mag.sh'
             # print(bashCommand)
