75899145c623c9cef7e4b929741ecc79e8a9cb1d
Ilya <ilyakudrov@yandex.ru>
2023-02-18 20:15:26 +0300

    flux_tube scripts updated

--- scripts/bash/do_flux_wilson.sh
+++ scripts/bash/do_flux_wilson.sh
@@ -3,20 +3,20 @@
 for((i=${conf_start};i<=${conf_end};i++))
 do
 
-if [[ ${conf_path_plaket_end} == "/" ]]; then
+if [[ ${conf_path_end_plaket} == "/" ]]; then
 
-conf_path_plaket_end=""
-
-fi
-
-if [[ ${conf_path_wilson_end} == "/" ]]; then
-
-conf_path_wilson_end=""
+conf_path_end_plaket=""
 
 fi
 
-conf_path_plaket="${conf_path_plaket_start}`printf %0${padding_plaket}d $i`${conf_path_plaket_end}"
-conf_path_wilson="${conf_path_wilson_start}`printf %0${padding_wilson}d $i`${conf_path_wilson_end}"
+if [[ ${conf_path_end_wilson} == "/" ]]; then
+
+conf_path_end_wilson=""
+
+fi
+
+conf_path_plaket="${conf_path_start_plaket}`printf %0${padding_plaket}d $i`${conf_path_end_plaket}"
+conf_path_wilson="${conf_path_start_wilson}`printf %0${padding_wilson}d $i`${conf_path_end_wilson}"
 
 if [ -f ${conf_path_plaket} ] && [ -s ${conf_path_plaket} ] && [ -f ${conf_path_wilson} ] && [ -s ${conf_path_wilson} ] ; then
 
@@ -25,11 +25,12 @@ output_path_electric="${output_path}/electric_`printf %04d $i`"
 output_path_magnetic="${output_path}/magnetic_`printf %04d $i`"
 
 parameters="-conf_format_plaket $conf_format_plaket -conf_format_wilson ${conf_format_wilson} -conf_path_plaket $conf_path_plaket\
- -conf_path_wilson ${conf_path_wilson} -bites_skip_plaket ${bites_skip_plaket} -bites_skip_wilson ${bites_skip_wilson}\
-  -matrix_type_plaket ${matrix_type_plaket} -matrix_type_wilson ${matrix_type_wilson} -output_path_electric ${output_path_electric} -output_path_magnetic ${output_path_magnetic}\
+   -conf_path_wilson ${conf_path_wilson} -bytes_skip_plaket ${bytes_skip_plaket} -bytes_skip_wilson ${bytes_skip_wilson}\
+   -matrix_type_plaket ${matrix_type_plaket} -matrix_type_wilson ${matrix_type_wilson} -output_path_electric ${output_path_electric} -output_path_magnetic ${output_path_magnetic}\
+   -convert_plaket ${convert_plaket} -convert_wilson ${convert_wilson}
    -T_min ${T_min} -T_max ${T_max} -R_min ${R_min} -R_max ${R_max} -L_spat ${L_spat} -L_time ${L_time} -x_trans ${x_trans}"
 
-/home/clusters/rrcmpi/kudrov/observables_cluster/code/exe/flux_wilson_${matrix_type_plaket}_${matrix_type_wilson} $parameters
+/home/clusters/rrcmpi/kudrov/observables_cluster/code/exe/flux_wilson_long_${matrix_type_plaket}_${matrix_type_wilson} $parameters
 
 fi
 
--- scripts/python/do1_flux_wilson.py
+++ scripts/python/do1_flux_wilson.py
@@ -7,135 +7,92 @@ from iterate_confs import distribute_jobs
 import subprocess
 import os
 
-#conf_size = "24^4"
-conf_size = "40^4"
-#conf_size = "32^4"
 #conf_type = "su2_suzuki"
 conf_type = "qc2dstag"
 theory_type = "su2"
-bites_skip_plaket = 0
-bites_skip_wilson = 0
 
-monopole_plaket = '/'
-monopole_wilson = '/'
-
-if monopole_plaket == '/':
-    monopole1 = theory_type
-else:
-    monopole1 = monopole_plaket
-
-if monopole_wilson == '/':
-    monopole2 = theory_type
-else:
-    monopole2 = monopole_wilson
-
-#HYP_alpha1 = "1"
-#HYP_alpha2 = "1"
-#HYP_alpha3 = "0.5"
-#APE_alpha = "0.5"
-#stout_alpha = "0.15"
-#APE_steps = "40"
-#HYP_steps = "0"
-
-R_min = 4
-R_max = 16
-T_min = 4
-T_max = 16
 x_trans = 0
 
 number_of_jobs = 100
 
-smearing = 'smeared'
+smearing_arr = ['HYP0_alpha=1_1_0.5_APE_alpha=0.5']
+decomposition_type_plaket_arr = ["original"]
+decomposition_type_wilson_arr = ["original"]
+beta_arr = ['/']
+mu_arr = ['mu0.00']
+conf_size_arr = ['40^4']
+additional_parameters_arr = ['steps_500/copies=3/compensate_1']
 
-for beta in ['/']:\
-        # for beta in ['beta2.4', 'beta2.5', 'beta2.6']:
-    # for beta in ['beta2.4']:
-    for mu in ['mu0.20']:
-        # for mu in ['mu0.30']:
-        # for mu in ['']:
+iter_arrays = [beta_arr, mu_arr, conf_size_arr,
+               decomposition_type_plaket_arr,
+               decomposition_type_wilson_arr, smearing_arr, additional_parameters_arr]
+for beta, mu, conf_size, decomposition_type_plaket, decomposition_type_wilson, smearing, additional_parameters in itertools.product(*iter_arrays):
 
-        f = open(
-            f'/home/clusters/rrcmpi/kudrov/smearing_cluster/smearing_parameters/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/{monopole_wilson}/smearing_flux.json')
-        data_smearing = json.load(f)
-        HYP_alpha1 = data_smearing['HYP_alpha1']
-        HYP_alpha2 = data_smearing['HYP_alpha2']
-        HYP_alpha3 = data_smearing['HYP_alpha3']
-        APE_alpha = data_smearing['APE_alpha']
-        APE = data_smearing['APE']
-        HYP = data_smearing['HYP']
-        APE_steps = data_smearing['APE_steps']
-        HYP_steps = data_smearing['HYP_steps']
+    f = open(
+        f'/home/clusters/rrcmpi/kudrov/conf/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/parameters_{decomposition_type_plaket}.json')
+    data_plaket = json.load(f)
+    conf_format_plaket = data_plaket['conf_format']
+    bytes_skip_plaket = data_plaket['bytes_skip']
+    matrix_type_plaket = data_plaket['matrix_type']
+    conf_path_start_plaket = data_plaket['conf_path_start']
+    conf_path_end_plaket = data_plaket['conf_path_end']
+    padding_plaket = data_plaket['padding']
+    conf_name_plaket = data_plaket['conf_name']
+    convert_plaket = data_plaket['convert']
+    L_spat = data_plaket['x_size']
+    L_time = data_plaket['t_size']
 
-        #HYP_alpha1 = "1"
-        #HYP_alpha2 = "1"
-        #HYP_alpha3 = "0.5"
-        #HYP_alpha1 = "0.75"
-        #HYP_alpha2 = "0.6"
-        #HYP_alpha3 = "0.3"
-        #APE_alpha = "0.5"
-        #stout_alpha = "0.15"
-        #APE_steps = "200"
-        #HYP_steps = "1"
+    R_min = 4
+    R_max = L_spat / 2
+    T_min = 4
+    T_max = L_time / 2
 
-        f = open(
-            f'/home/clusters/rrcmpi/kudrov/conf/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/{monopole_plaket}/parameters.json')
-        data_plaket = json.load(f)
+    f = open(
+        f'/home/clusters/rrcmpi/kudrov/conf/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/parameters_{decomposition_type_wilson}.json')
+    data_wilson = json.load(f)
+    conf_format_wilson = data_wilson['conf_format']
+    bytes_skip_wilson = data_wilson['bytes_skip']
+    matrix_type_wilson = data_wilson['matrix_type']
+    conf_path_start_wilson = data_wilson['conf_path_start']
+    conf_path_end_wilson = data_wilson['conf_path_end']
+    padding_wilson = data_wilson['padding']
+    conf_name_wilson = data_wilson['conf_name']
+    convert_wilson = data_wilson['convert']
 
-        conf_format_plaket = data_plaket['conf_format']
-        bites_skip_plaket = data_plaket['bites_skip']
-        matrix_type_plaket = data_plaket['matrix_type']
-        L_spat = data_plaket['x_size']
-        L_time = data_plaket['t_size']
-        conf_path_plaket_start = data_plaket['conf_path_start']
-        conf_path_plaket_end = data_plaket['conf_path_end']
-        padding_plaket = data_plaket['padding']
-        conf_name_plaket = data_plaket['conf_name']
+    conf_path_start = f'/home/clusters/rrcmpi/kudrov/smearing/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/{decomposition_type_wilson}/{smearing}/{additional_parameters}'
+    conf_name = 'smeared_'
+    conf_path_end = '/'
+    conf_format = 'double'
+    padding = 4
+    bytes_skip = 0
+    convert = 0
 
-        f = open(
-            f'/home/clusters/rrcmpi/kudrov/conf/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/{monopole_wilson}/parameters.json')
-        data_wilson = json.load(f)
-        conf_format_wilson = data_wilson['conf_format']
-        bites_skip_wilson = data_wilson['bites_skip']
-        matrix_type_wilson = data_wilson['matrix_type']
-        conf_path_wilson_start = data_wilson['conf_path_start']
-        conf_path_wilson_end = data_wilson['conf_path_end']
-        padding_wilson = data_wilson['padding']
-        conf_name_wilson = data_wilson['conf_name']
+    #chains = {'/': [201, 201]}
+    #jobs = distribute_jobs(chains, number_of_jobs)
+    jobs = distribute_jobs(data_plaket['chains'], number_of_jobs)
 
-        if smearing == 'smeared':
-            conf_format_wilson = 'double'
-            bites_skip_wilson = '0'
-            conf_path_wilson_start = f'/home/clusters/rrcmpi/kudrov/smearing_cluster/confs_smeared/{theory_type}/'\
-                f'{conf_type}/{conf_size}/{beta}/{mu}/{monopole_wilson}/HYP{HYP_steps}_alpha={HYP_alpha1}_{HYP_alpha2}_{HYP_alpha3}_APE{APE_steps}_alpha={APE_alpha}'
-            conf_path_wilson_end = '/'
-            padding_wilson = 4
-            conf_name_wilson = 'conf_'
+    for job in jobs:
 
-        #chains = {'/': [201, 201]}
-        #jobs = distribute_jobs(chains, number_of_jobs)
-        jobs = distribute_jobs(data_plaket['chains'], number_of_jobs)
+        log_path = f'/home/clusters/rrcmpi/kudrov/observables_cluster/logs/flux_tube_wilson/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/'\
+            f'/{decomposition_type_plaket}-{decomposition_type_wilson}/{smearing}/{job[0]}'
+        conf_path_start_plaket1 = f'{conf_path_start_plaket}/{job[0]}/{conf_name_plaket}'
+        conf_path_start_wilson1 = f'{conf_path_start_wilson}/{job[0]}/{conf_name_wilson}'
+        try:
+            os.makedirs(log_path)
+        except:
+            pass
 
-        for job in jobs:
+        output_path = f'/home/clusters/rrcmpi/kudrov/observables_cluster/result/flux_tube_wilson/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/{decomposition_type_plaket}-{decomposition_type_wilson}'\
+            f'/{smearing}/{job[0]}'
 
-            log_path = f'/home/clusters/rrcmpi/kudrov/observables_cluster/logs/flux_tube_wilson/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/{monopole1}-{monopole2}'\
-                f'/HYP{HYP_steps}_alpha={HYP_alpha1}_{HYP_alpha2}_{HYP_alpha3}_APE{APE_steps}_alpha={APE_alpha}/{job[0]}'
-            conf_path_plaket_start1 = f'{conf_path_plaket_start}/{job[0]}/{conf_name_plaket}'
-            conf_path_wilson_start1 = f'{conf_path_wilson_start}/{job[0]}/{conf_name_wilson}'
-            try:
-                os.makedirs(log_path)
-            except:
-                pass
-
-            output_path = f'/home/clusters/rrcmpi/kudrov/observables_cluster/result/flux_tube_wilson/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/{monopole1}-{monopole2}'\
-                f'/HYP{HYP_steps}_alpha={HYP_alpha1}_{HYP_alpha2}_{HYP_alpha3}_APE{APE_steps}_alpha={APE_alpha}/{job[0]}'
-
-            bashCommand = f'qsub -q long -v conf_format_plaket={conf_format_plaket},conf_format_wilson={conf_format_wilson},bites_skip_plaket={bites_skip_plaket},'\
-                f'bites_skip_wilson={bites_skip_wilson},matrix_type_plaket={matrix_type_plaket},matrix_type_wilson={matrix_type_wilson},'\
-                f'conf_path_plaket_start={conf_path_plaket_start1},conf_path_plaket_end={conf_path_plaket_end},conf_path_wilson_start={conf_path_wilson_start1},conf_path_wilson_end={conf_path_wilson_end},'\
-                f'padding_plaket={padding_plaket},padding_wilson={padding_wilson},R_min={R_min},R_max={R_max},T_min={T_min},T_max={T_max},x_trans={x_trans},L_spat={L_spat},L_time={L_time},'\
-                f'output_path={output_path},chain={job[0]},conf_start={job[1]},conf_end={job[2]}'\
-                f' -o {log_path}/{job[1]:04}-{job[2]:04}.o -e {log_path}/{job[1]:04}-{job[2]:04}.e ../bash/do_flux_wilson.sh'
-            # print(bashCommand)
-            process = subprocess.Popen(bashCommand.split())
-            output, error = process.communicate()
-            #print(output, error)
+        bashCommand = f'qsub -q long -v conf_format_plaket={conf_format_plaket},conf_format_wilson={conf_format_wilson},bytes_skip_plaket={bytes_skip_plaket},'\
+            f'bytes_skip_wilson={bytes_skip_wilson},matrix_type_plaket={matrix_type_plaket},matrix_type_wilson={matrix_type_wilson},'\
+            f'conf_path_start_plaket={conf_path_start_plaket1},conf_path_end_plaket={conf_path_end_plaket},conf_path_start_wilson={conf_path_start_wilson1},conf_path_end_wilson={conf_path_end_wilson},'\
+            f'padding_plaket={padding_plaket},padding_wilson={padding_wilson},convert_plaket={convert_plaket},convert_wilson={convert_wilson},'\
+            f'R_min={R_min},R_max={R_max},T_min={T_min},T_max={T_max},x_trans={x_trans},L_spat={L_spat},L_time={L_time},'\
+            f'output_path={output_path},chain={job[0]},conf_start={job[1]},conf_end={job[2]}'\
+            f' -o {log_path}/{job[1]:04}-{job[2]:04}.o -e {log_path}/{job[1]:04}-{job[2]:04}.e ../bash/do_flux_wilson.sh'
+        # print(bashCommand)
+        process = subprocess.Popen(bashCommand.split())
+        output, error = process.communicate()
+        #print(output, error)
