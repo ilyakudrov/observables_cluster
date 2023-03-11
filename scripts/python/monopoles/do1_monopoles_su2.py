66e52b02b2397fe6645fa626a573390dce4cb589
Ilya <ilyakudrov@yandex.ru>
2023-03-11 12:42:02 +0300

    updated monopole scripts

--- scripts/python/monopoles/do1_monopoles_su2.py
+++ scripts/python/monopoles/do1_monopoles_su2.py
@@ -1,10 +1,12 @@
 import sys
 import json
+import itertools
+import subprocess
+import os
+
 sys.path.append(os.path.join(os.path.dirname(
     os.path.abspath(__file__)), "..", "..", "..", "lib", "src", "python"))
 from iterate_confs import distribute_jobs
-import subprocess
-import os
 
 L_spat = 48
 L_time = 48
@@ -16,53 +18,54 @@ theory_type = "su2"
 
 arch = "rrcmpi-a"
 
-additional_parameters = 'T_step=0.0005/T_final=0.5/OR_steps=4'
-
+beta_arr = ['beta2.8']
+#mu_arr = ['mu0.00', 'mu0.05', 'mu0.20', 'mu0.25', 'mu0.30', 'mu0.35', 'mu0.45']
+mu_arr = ['/']
+additional_parameters_arr = ['steps_500/copies=1']
+conf_size_arr = ['36^4']
 
 number_of_jobs = 50
 
-# for beta in ['/']:
-# for beta in ['beta6.3']:
-# for beta in ['beta2.5', 'beta2.6']:
-for beta in ['beta2.7']:
-    # for mu in ['mu0.00', 'mu0.05', 'mu0.20', 'mu0.25', 'mu0.30', 'mu0.35', 'mu0.45']:
-    # for mu in ['mu0.05', 'mu0.45']:
-    for mu in ['/']:
+iter_arrays = [beta_arr, mu_arr, conf_size_arr, additional_parameters_arr]
+for beta, mu, conf_size, additional_parameters in itertools.product(*iter_arrays):
 
-        f = open(
-            f'/home/clusters/rrcmpi/kudrov/conf/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/parameters.json')
-        data = json.load(f)
+    f = open(
+        f'/home/clusters/rrcmpi/kudrov/conf/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/parameters_mag.json')
+    data = json.load(f)
+    conf_format = data['conf_format']
+    bytes_skip = data['bytes_skip']
+    matrix_type = data['matrix_type']
+    conf_path_start = data['conf_path_start']
+    conf_path_end = data['conf_path_end']
+    padding = data['padding']
+    conf_name = data['conf_name']
+    L_spat = data['x_size']
+    L_time = data['t_size']
 
-        conf_format = "double"
-        bytes_skip = 0
-        matrix_type = 'su2'
-        conf_path_start = f'/home/clusters/rrcmpi/kudrov/mag/conf_mag/su2/{conf_type}/{conf_size}/{beta}/{additional_parameters}'
-        conf_path_end = "/"
-        padding = 4
-        conf_name = "conf_"
+    conf_path_start = conf_path_start + f'/{additional_parameters}'
 
-        chains = {'/': [1, 50]}
-        #chains = {'s0': [201, 250]}
-        jobs = distribute_jobs(chains, number_of_jobs)
-        #jobs = distribute_jobs(data['chains'], number_of_jobs)
+    # chains = {'/': [1, 50]}
+    #chains = {'s0': [201, 250]}
+    # jobs = distribute_jobs(chains, number_of_jobs)
+    jobs = distribute_jobs(data['chains'], number_of_jobs)
 
-        for job in jobs:
+    for job in jobs:
 
-            log_path = f'/home/clusters/rrcmpi/kudrov/observables_cluster/logs/monopoles_su2/{conf_type}/{conf_size}/{beta}/{mu}/'\
-                f'{additional_parameters}/{job[0]}'
-            conf_path_start1 = f'{conf_path_start}/{job[0]}/{conf_name}'
-            try:
-                os.makedirs(log_path)
-            except:
-                pass
-            output_path = f'/home/clusters/rrcmpi/kudrov/observables_cluster/result/monopoles_su2/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/{additional_parameters}'
-            # for nt8 and bigger
-            # qsub -q mem4gb -l nodes=1:ppn=2
-            bashCommand = f'qsub -q long -v conf_path_start={conf_path_start1},conf_path_end={conf_path_end},padding={padding},conf_format={conf_format},bytes_skip={bytes_skip},'\
-                f'output_path={output_path},arch={arch},'\
-                f'L_spat={L_spat},L_time={L_time},chain={job[0]},conf_start={job[1]},conf_end={job[2]}'\
-                f' -o {log_path}/{job[1]:04}-{job[2]:04}.o -e {log_path}/{job[1]:04}-{job[2]:04}.e ../../bash/monopoles/do_monopoles_su2.sh'
-            # print(bashCommand)
-            process = subprocess.Popen(bashCommand.split())
-            output, error = process.communicate()
-            # print(output, error)
+        log_path = f'/home/clusters/rrcmpi/kudrov/observables_cluster/logs/monopoles_su2/{conf_type}/{conf_size}/{beta}/{mu}/'\
+            f'{additional_parameters}/{job[0]}'
+        conf_path_start1 = f'{conf_path_start}/{job[0]}/{conf_name}'
+        try:
+            os.makedirs(log_path)
+        except:
+            pass
+        output_path = f'/home/clusters/rrcmpi/kudrov/observables_cluster/result/monopoles_su2/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/{additional_parameters}'
+        # for nt8 and bigger
+        # qsub -q mem4gb -l nodes=1:ppn=2
+        bashCommand = f'qsub -q long -v conf_path_start={conf_path_start1},conf_path_end={conf_path_end},padding={padding},conf_format={conf_format},bytes_skip={bytes_skip},'\
+            f'output_path={output_path},arch={arch},'\
+            f'L_spat={L_spat},L_time={L_time},chain={job[0]},conf_start={job[1]},conf_end={job[2]}'\
+            f' -o {log_path}/{job[1]:04}-{job[2]:04}.o -e {log_path}/{job[1]:04}-{job[2]:04}.e ../../bash/monopoles/do_monopoles_su2.sh'
+        # print(bashCommand)
+        process = subprocess.Popen(bashCommand.split())
+        output, error = process.communicate()
+        # print(output, error)
