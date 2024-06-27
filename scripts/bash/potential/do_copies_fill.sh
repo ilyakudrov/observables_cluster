1f0fce0614da1c10f0f5cef84580616705716f1b
ilya <ilyakudrov@yandex.ru>
2024-06-28 00:55:34 +0300

    working changes

--- scripts/bash/potential/do_copies_fill.sh
+++ scripts/bash/potential/do_copies_fill.sh
@@ -1,2 +1,2 @@
-#!bin/bash
+#!/bin/bash
 python3 /home/clusters/rrcmpi/kudrov/observables/code/python/potential_copy_fill.py ${parameters[@]}
--- scripts/python/potential/do1_copies_fill.py
+++ scripts/python/potential/do1_copies_fill.py
@@ -4,8 +4,7 @@ import os
 
 conf_type = 'gluodynamics'
 #conf_type = "su2_suzuki"
-#conf_type = "gluodynamics"
-conf_type = 'QCD/140MeV'
+#conf_type = 'QCD/140MeV'
 #conf_type = "qc2dstag"
 theory_type = 'su3'
 decomposition_type_arr = ['monopole']
@@ -22,7 +21,8 @@ beta_arr = ['beta6.0']
 mu_arr = ['/']
 #mu_arr = ['mu0.05', 'mu0.20', 'mu0.25', 'mu0.30', 'mu0.35', 'mu0.40', 'mu0.45']
 #mu_arr = ['mu0.35', 'mu0.40']
-conf_size_arr = ['16^4', '24^4', '32^4']
+#conf_size_arr = ['16^4', '24^4', '32^4']
+conf_size_arr = ['16^4']
 #conf_size_arr = ['nt4', 'nt6', 'nt8', 'nt10', 'nt12', 'nt14', 'nt16', 'nt18', 'nt20']
 iter_arrays = [beta_arr, mu_arr, conf_size_arr,
                additional_parameters_arr, decomposition_type_arr, smearing_param_arr]
@@ -34,12 +34,14 @@ for beta, mu, conf_size, additional_parameters, decomposition_type, smearing_par
     except:
         pass
     base_path = '/home/clusters/rrcmpi/kudrov/observables_cluster/result'
-    base_path_out = '/home/clusters/rrcmpi/kudrov/observables/data'
-    path_params = f'{smearing}/{operator_type}/{representation}/{axis}\
-                    /{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/{decomposition_type}\
-                    /{smearing_param}/{additional_parameters}'
-    parameters = f'--base_path {base_path} --base_path_out {base_path_out} --path_params {path_params} --copies {gauge_copies}'
-    bashCommand = f'qsub -q long -v parameters=\"${parameters}\"'\
-        f' -o {log_path}/{decomposition_type}.o -e {log_path}/{decomposition_type}.e ../../bash/potential/do_copies_fill.sh'
-process = subprocess.Popen(bashCommand.split())
-output, error = process.communicate()
+    path_output_base = '/home/clusters/rrcmpi/kudrov/observables/data'
+    path_params = f'{smearing}/{operator_type}/{representation}/{axis}'\
+                    f'/{theory_type}/{conf_type}/{conf_size}/{beta}/{mu}/{decomposition_type}'\
+                    f'/{smearing_param}/{additional_parameters}'
+    parameters = f'--base_path {base_path} --path_output_base {path_output_base} --path_params {path_params} --copies {gauge_copies}'
+    command_qsub = f'qsub -q long -v'
+    command_parameters = f'parameters={parameters}'
+    command_script = f'-o {log_path}/{decomposition_type}.o -e {log_path}/{decomposition_type}.e ../../bash/potential/do_copies_fill.sh'
+    command = command_qsub.split() + [command_parameters] + command_script.split()
+    process = subprocess.Popen(command)
+    output, error = process.communicate()
