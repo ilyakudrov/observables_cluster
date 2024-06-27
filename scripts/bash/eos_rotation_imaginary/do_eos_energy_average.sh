64cc2d2e6bf7baff42e6e34ef8976ce458d48f5a
Ilya <ilyakudrov@yandex.ru>
2024-06-28 00:58:14 +0300

    added scrits for processing acerage enegry for eos_rotation_imaginary

---
+++ scripts/bash/eos_rotation_imaginary/do_eos_energy_average.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/home/clusters/rrcmpi/kudrov/observables/code/python/eos_rotation_imaginary/python3 average.py ${parameters[@]}
---
+++ scripts/python/eos_rotation_imaginary/do1_eos_energy_average.py
@@ -0,0 +1,36 @@
+import itertools
+import sys
+import subprocess
+import os
+
+def get_dir_names(path: str) -> list[str]:
+    """Get names of subdirectories in path.
+
+    Args:
+        path: path where to find subdirectories
+
+    Returns: list of names of the subdirectories
+    """
+    directories = []
+    for (dirpath, dirnames, filenames) in os.walk(path):
+        directories.extend(dirnames)
+        break
+    return directories
+
+lattice_size_arr = ["7x42x169sq"]
+boundary_arr=["OBCb_cV"]
+base_path="/home/clusters/rrcmpi/kudrov/eos_high_precision/result/logs"
+iter_arrays = [lattice_size_arr, boundary_arr]
+for lattice_size, boundary in itertools.product(*iter_arrays):
+    velocity_dirs = get_dir_names(f'{base_path}/{lattice_size}/{boundary}')
+    for velocity in velocity_dirs:
+        beta_dirs = get_dir_names(f'{base_path}/{lattice_size}/{boundary}/{velocity}')
+        for beta in beta_dirs:
+            log_path = f'/home/clusters/rrcmpi/kudrov/observables_cluster/logs/eos_energy_average/{lattice_size}/{boundary}/{velocity}/{beta}'
+            parameters=f'--base_path ${base_path} --lattice_size ${lattice_size} --velocity ${velocity} --boundary ${boundary} --beta ${beta}'
+            command_qsub = f'qsub -q mem16gb -l nodes=1:ppn=8 -v'
+            command_parameters = f'parameters={parameters}'
+            command_script = f'-o {log_path}/energy_average.o -e {log_path}/energy_average.e ../../bash/eos_rotation_imaginary/do_eos_energy_average.sh'
+            command = command_qsub.split() + [command_parameters] + command_script.split()
+            process = subprocess.Popen(command)
+            output, error = process.communicate()
